31ce733322f5c3390c418ecbcc6d721d51ce70ea
pedro <pedropkmmt@gmail.com>
2023-10-06 15:00:03 -0700

    for

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+include main(void)
+{
+	int i;
+	char n;
+	for (i = 0; i < 10 ; i++)
+	putchar(i + '0');
+	for (n = 'a'; n <= 'f' ; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
