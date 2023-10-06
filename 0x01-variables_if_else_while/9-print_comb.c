cf8f0505384195a50a3baba4cb4f6e57dc20926d
pedro <pedropkmmt@gmail.com>
2023-10-06 15:10:17 -0700

    for

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	putchar(i);
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
