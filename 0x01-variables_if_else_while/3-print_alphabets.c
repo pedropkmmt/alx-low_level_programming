e3e13aaa9df8e8f08b72ed1440621a33fae9c73f
pedro <pedropkmmt@gmail.com>
2023-10-06 14:02:13 -0700

    for

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-	char i:
+	char i;
 	
 	for (i = 'a'; i <= 'z'; i++)
-	putchar(i):
+	putchar(i);
 	for (i = 'A'; i <= 'Z'; i++)
 	putchar(i):
-        putchar('\n'):
+        putchar('\n');
         return (0);
 }
