1dde17601c1d0cc8e586d9cd20a41d2e9509464f
pedro <pedropkmmt@gmail.com>
2023-10-10 11:43:27 -0700

    vlaue

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <ctype.h>
 #include <stdio.h>
+/**
+ * add - adds two vlaues
+ * Return: returns int values
+ */
 int add(int a, int b)
 {
-	   return(a + b);
+	return(a + b);
 }
