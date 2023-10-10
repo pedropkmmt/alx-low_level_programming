eb03c817afe1ca90720af6a3ab084a828410df16
pedro <pedropkmmt@gmail.com>
2023-10-10 03:24:27 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ *print_alphabet - prints alphabet a to z in small letters
+ *Return: Always 0.
+ */
 void print_alphabet(void)
 {
 	char i;
