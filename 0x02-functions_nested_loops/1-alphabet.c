42dfab5896f577d07d2951e695b1583c321ae143
pedro <pedropkmmt@gmail.com>
2023-10-10 03:11:43 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a'; i <= z; i++)
+	 _putchar(i);
+	_putchar('\n')
+}
+}
