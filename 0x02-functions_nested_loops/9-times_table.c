44f71c486ad773a9b574fc9cdaa4ec30ea70ffec
pedro <pedropkmmt@gmail.com>
2023-10-10 11:21:29 -0700

    vlaue

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,21 +6,21 @@ void times_table(void)
 	int i;
 	int a;
 	int result;
-
-	for (i = 0; i < 10; i++)
+	
+	for (i = 0; i < 10 ; i++)
 	{
-	for ( a = 0; a < 10; a++)
-	{
-	result = i * a;
-	if (a == 0)
-		printf("%d", result);
-	else
-	{
-		printf("%2d", result);
-		if (a != 0)
-			printf(", ");
+		for (a = 0 ; a < 10 ; a++)
+		{
+			result = i * a;
+			if (a == 0)
+				printf("%d, " result);
+			else 
+			{
+				printf("%2d" , result);
+				if (a != 0)
+					printf(", ");
+			}
+		}
+		printf("\n");
 	}
-}
-}
-printf('\n');
 }
