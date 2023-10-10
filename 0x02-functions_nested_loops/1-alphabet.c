71158327fa1144008ec1168de4e16d62a4cbbb77
pedro <pedropkmmt@gmail.com>
2023-10-10 03:16:40 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,14 @@
  *   *
  *    * Return: Always 0.
  *     */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= z; i++)
+{
 	 _putchar(i);
-	_putchar('\n')
+}
+	_putchar('\n');
 }
 
