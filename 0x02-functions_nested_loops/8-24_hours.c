b1dcd82445f47507c969115941130103b00c72bf
pedro <pedropkmmt@gmail.com>
2023-10-10 10:07:41 -0700

    vlaue

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * jack_bauer - prints minute of the day
+ * Return: return void
+ */
+void jack_bauer(void)
+{
+	int i, a;
+
+	for (i = 0; i < 24 ; i++)
+	{
+		for (a = 0; a < 60; a++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+		}
+	}
+}
