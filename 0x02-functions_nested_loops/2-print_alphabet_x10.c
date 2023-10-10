aa4fce64d136bd1714cf77a496672c5c198cd65e
pedro <pedropkmmt@gmail.com>
2023-10-10 03:51:39 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
-	for(ch = 0; ch < 10; ch++)
+	for (ch = 0; ch < 10; ch++)
 {
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
 }
