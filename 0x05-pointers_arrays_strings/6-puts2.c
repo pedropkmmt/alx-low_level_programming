f3ee1c6cb79eecda8ad4a5830d5bf9909f77df04
pedro <pedropkmmt@gmail.com>
2023-10-16 07:13:16 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts2 - prints one character
+ * @str: string
+ * Return: string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int k = 0;
+	char *m = str;
+	int p;
+
+	while (*m != '\0')
+	{
+		m++;
+		i++;
+	}
+	k = i - 1;
+	for (p = 0; p <= k; p++)
+	{
+		if (p % 2 == 0)
+	_putchar(str[p]);
+	}
+	_putchar('\n');
+}
