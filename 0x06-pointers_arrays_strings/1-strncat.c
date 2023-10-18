c273a4ac1053e6a2cae58664467e1264f22989a1
pedro <pedropkmmt@gmail.com>
2023-10-18 15:28:47 -0700

    join

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - joins two strings
+ * @dest: value
+ * @src: value
+ * @n: value
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int k;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++
+	k = 0;
+	while (k < n && src[k] != '\0')
+	{
+		dest[i] = src[k];
+		i++;
+		k++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
