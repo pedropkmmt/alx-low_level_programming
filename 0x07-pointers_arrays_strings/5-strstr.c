c79e751a38418fe6be32b5b05029ea809a5dbbb6
pedro <pedropkmmt@gmail.com>
2023-10-23 10:14:43 -0700

    string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -20,15 +20,13 @@ char *_strstr(char *haystack, char *needle)
 
 	if (haystack[index] == needle[index])
 		{
-			do 
-			{
-				if (needle[index + 1] == '\0')
-				{
+	do {
+	if (needle[index + 1] == '\0')
+		{
 					return (haystack);
-				}
+		}
 				index++;
-			}
-			while (haystack[index] == needle[index]);
+			} while (haystack[index] == needle[index]);
 		}
 		haystack++;
 	}
