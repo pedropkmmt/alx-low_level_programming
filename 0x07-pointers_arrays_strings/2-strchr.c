dba6e52404fa17a57c9b98e20e616118f428f291
pedro <pedropkmmt@gmail.com>
2023-10-23 02:47:45 -0700

    find character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ *_strchr - locates a character
+ *@s: string to be searched
+ *@c: character to be loacted
+ *Return: if c is found - pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+	return ('\0');
+}
