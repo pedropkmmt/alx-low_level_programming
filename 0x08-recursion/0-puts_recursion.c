5e08ed9b06604661c7a693122baa463041f85d7d
pedro <pedropkmmt@gmail.com>
2023-10-25 04:02:20 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _put_recursion - function
+ * @s: parameter
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar (*s);
+		_putchar_recursions(s + 1);
+	}
+	else 
+		_putchar ('\n');
+
+}
