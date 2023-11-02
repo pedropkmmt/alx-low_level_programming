8a16d288b4ade6275f0a300f8916330f5f43097a
pedro <pedropkmmt@gmail.com>
2023-11-02 13:50:20 -0700

    malloc free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,8 +14,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	 i= calloc(nmemb, size);
-	if (output == NULL)
+	if (i == NULL)
 		return (NULL);
 	else
-		return (output);
+		return (i);
 }
