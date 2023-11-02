51f9f117f2fe80b3736b38941bf0830c65f78f7e
pedro <pedropkmmt@gmail.com>
2023-11-02 13:14:03 -0700

    malloc free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - main function
+ * @nmemb: number of array
+ * @size: size of array
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *output;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	output = calloc(nmemb, size);
+	if (ouput == NULL)
+		return (NULL)
+	else
+		return (output);
+}
