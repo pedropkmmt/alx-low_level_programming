bffdcd21371e6d6485c11dfbc6b00e5afd9fc0b6
pedro <pedropkmmt@gmail.com>
2023-11-06 10:35:53 -0800

    structure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,11 +9,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog my_dog;
-
-	my_dog.name = "Poppy";
-	my_dog.age = 4;
-	my_dog.owner = "Bob";
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-	return (0);
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age =age;
+		d->owner = owner;
 }
