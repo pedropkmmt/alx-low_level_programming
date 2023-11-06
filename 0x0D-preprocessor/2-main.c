e0c6495be8ed04b02f5cba14c75326e64db41d5b
pedro <pedropkmmt@gmail.com>
2023-11-06 06:33:43 -0800

    header

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 
 int main(void)
 {
     printf("This program was compiled from %s\n", __FILE__);
-    return 0;
+    return (0);
 }
