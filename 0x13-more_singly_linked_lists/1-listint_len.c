5ff4156257ac2af0eae0d64416b1ca0bd2923aa1
pedro <pedropkmmt@gmail.com>
2023-11-20 10:06:47 -0800

    linked lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - main function
+ * @h: linked list 
+ * Return: nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
+
