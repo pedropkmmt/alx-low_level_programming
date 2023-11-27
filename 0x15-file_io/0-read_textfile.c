f82d39af69df988f01064f051d84a929e565cf69
pedro <pedropkmmt@gmail.com>
2023-11-27 10:03:31 -0800

    file i o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * read_textfile - main function
+ * @filename: file to read
+ * @letters: number of letters
+ * Return: number of letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_d;
+	ssize_t lenr, lenw;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	file_d = open(filename, O_RDONLY);
+	if (file_d == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(file_d);
+		return (0);
+	}
+	lenr = read(file_d, buffer, letters);
+	close(file_d);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
+}
